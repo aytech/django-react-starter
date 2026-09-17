2c80974ab5338867cc1b5fa9a3297280dda54bd8
Oleg <oyapparov@gmail.com>
2026-09-17 22:19:11 +0200

    Fix Docker build job

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Install locked frontend dependencies, build React, and collect Django static files.
-set -eu
-cd "$(dirname "$0")"
-
-npm --prefix client ci
-npm --prefix client run build
-"${PYTHON:-python}" manage.py collectstatic --noinput
